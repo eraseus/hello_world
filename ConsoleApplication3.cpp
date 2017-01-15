3441e6083fd9ff023c7830718da1ef86d0703867
eraseus <please_die@list.ru>
2017-01-15 22:56:45 +0300

    udaleno

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+int a = 4;
+int b = 7;
+int c;
+int funcrion();
+int main()
+{
+	funcrion();
+	c = a;
+	return c;
+	}
+
+int funcrion()
+{
+	a = b++;
+
+	return a;
+}
